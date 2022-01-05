53eb3001291e749e072308d456ead0714a5b6ead
Max <maksppap123@mail.ru>
2022-01-05 04:18:33 +0300

    predict model

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 docker exec -it flask-hello_flask_1 bash
+
+
+docker exec -it flask-baseline_flask python trainmodel.py
--- hello.py
+++ hello.py
@@ -1,7 +1,41 @@
 from flask import Flask
+import joblib
+import numpy as np
 app = Flask(__name__)
+
+
+knn = joblib.load('knn.pkl')
 
 @app.route('/')
 def hello_world():
     print(1+2)
     return 'Hello, d'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers),1)
+
+
+@app.route('/avg/<nums>')
+def avg(nums):
+    nums= nums.split(',')
+    nums= [float(num) for num in nums]
+    num_mean = np.mean(nums)
+    print(f'{num_mean}')
+
+    return str(num_mean)
+
+
+
+@app.route('/iris/<param>')
+def iris(param):
+    param= param.split(',')
+    param= [float(num) for num in param]
+    param = np.array(param).reshape(1,-1)
+    predict = knn.predict(param) 
+
+    return str(predict)
